3bef24b34ee2e1f7c152ed637144919eecea847c
Thanh Vo <thanhvo.dev@gmail.com>
2020-12-02 14:40:34 +0700

    Add git ignore

--- Statistics/baitaplonXacSuat.R
+++ Statistics/baitaplonXacSuat.R
@@ -1,3 +1,8 @@
+#A.Hoi quy tuyen tinh
+#Bai tap 1:
+#1.
 giaNha <- read.csv("C:\\Users\\Thanh Vo\\Desktop\\BaiTapLon\\gia_nha.csv", header = TRUE)
+#2a.
 new_DF <- giaNha[, c(6, 9, 11, 14, 16, 23)]
-
+#2b
+new_DF <- na.omit(new_DF)
