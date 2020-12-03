16a20da66c3defee19c3ec74316d6b495a0d2997
Thanh Vo <thanhvo.dev@gmail.com>
2020-12-03 14:19:54 +0700

    Add 3b,c exercises

--- Statistics/baitaplonXacSuat.R
+++ Statistics/baitaplonXacSuat.R
@@ -6,3 +6,19 @@ giaNha <- read.csv("C:\\Users\\Thanh Vo\\Desktop\\BaiTapLon\\gia_nha.csv", heade
 new_DF <- giaNha[, c(6, 9, 11, 14, 16, 23)]
 #2b
 new_DF <- na.omit(new_DF)
+#3a
+new_DF$price <- log(new_DF$price)
+new_DF$sqft_living15 <- log(new_DF$sqft_living15)
+new_DF$sqft_above <- log(new_DF$sqft_above)
+new_DF$sqft_living <- log(new_DF$sqft_living)
+#3b
+price <- c(mean(new_DF$price), median(new_DF$price), sd(new_DF$price), min(new_DF$price), max(new_DF$price))
+sqft_living15 <- c(mean(new_DF$sqft_living15), median(new_DF$sqft_living15), sd(new_DF$sqft_living15), min(new_DF$sqft_living15), max(new_DF$sqft_living15))
+sqft_above <- c(mean(new_DF$sqft_above), median(new_DF$sqft_above), sd(new_DF$sqft_above), min(new_DF$sqft_above), max(new_DF$sqft_above))
+sqft_living <- c(mean(new_DF$sqft_living), median(new_DF$sqft_living), sd(new_DF$sqft_living), min(new_DF$sqft_living), max(new_DF$sqft_living))
+table <- data.frame(price, sqft_living15, sqft_above, sqft_living)
+row.names(table) <- c("mean", "median", "sd", "min", "max")
+#3c
+new_DF$floors
+new_DF$floors <- factor(new_DF$floors)
+summary(new_DF$floors)
