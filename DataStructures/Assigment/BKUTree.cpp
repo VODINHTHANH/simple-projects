6703522f2063d4f56c3fe4a68ce3cf62b53a52ac
Thanh Vo <thanhvo.dev@gmail.com>
2020-12-05 10:06:17 +0700

    add search traverlist

--- DataStructures/Assigment/BKUTree.cpp
+++ DataStructures/Assigment/BKUTree.cpp
@@ -123,7 +123,9 @@ public:
         }
         else if (isPresentInQueue(key))
         {
-            splay->search(key);
+            typename SplayTree::Node *p = splay->searchTrav(splay->root, key, traversedList);
+            splay->splayOne(p);
+            return p->entry->value;
         }
         else
         {
@@ -177,11 +179,12 @@ public:
             Node *parent;
             typename AVLTree::Node *corr;
 
-            Node(Entry *entry = NULL, Node *left = NULL, Node *right = NULL)
+            Node(Entry *entry = NULL, Node *left = NULL, Node *right = NULL, Node *parent = NULL)
             {
                 this->entry = entry;
                 this->left = left;
                 this->right = right;
+                this->parent = parent;
                 this->corr = NULL;
             }
         };
@@ -225,11 +228,15 @@ public:
                 y->right->parent = x;
             }
             y->parent = x->parent;
+            Node *k = x->parent;
+            Node *t;
+            if (k != NULL)
+                t = k->right;
             if (x->parent == NULL)
             { //x is root
                 root = y;
             }
-            else if (x == x->parent->right)
+            else if (x == t)
             { //x is left child
                 x->parent->right = y;
             }
@@ -239,6 +246,47 @@ public:
             }
             y->right = x;
             x->parent = y;
+        }
+        void splayOne(Node *n)
+        {
+            //node is not root
+            if (n->parent == root)
+            { //node is child of root, one rotation
+                if (n == n->parent->left)
+                {
+                    right_rotate(n->parent);
+                }
+                else
+                {
+                    left_rotate(n->parent);
+                }
+            }
+            else
+            {
+                Node *p = n->parent;
+                Node *g = p->parent; //grandparent
+
+                if (n->parent->left == n && p->parent->left == p)
+                { //both are left children
+                    right_rotate(g);
+                    right_rotate(p);
+                }
+                else if (n->parent->right == n && p->parent->right == p)
+                { //both are right children
+                    left_rotate(g);
+                    left_rotate(p);
+                }
+                else if (n->parent->right == n && p->parent->left == p)
+                {
+                    left_rotate(p);
+                    right_rotate(g);
+                }
+                else if (n->parent->left == n && p->parent->right == p)
+                {
+                    right_rotate(p);
+                    left_rotate(g);
+                }
+            }
         }
         void splay(Node *n)
         {
@@ -326,6 +374,18 @@ public:
 
             return res2;
         }
+        Node *searchTrav(Node *rt, int key, vector<K> &traverseList)
+        {
+            if (rt == NULL)
+                return NULL;
+            if (key == rt->entry->key)
+                return rt;
+            traverseList.push_back(rt->entry->key);
+            if (key < rt->entry->key)
+                return searchTrav(rt->left, key, traverseList);
+
+            return searchTrav(rt->right, key, traverseList);
+        }
         Node *add(Entry *&entry)
         {
             if (isPresent(root, entry->key) != NULL)
@@ -385,13 +445,14 @@ public:
         V search(K key)
         {
             Node *n = isPresent(root, key);
+
             if (n == NULL)
             {
                 throw runtime_error("Not found");
             }
             else
             {
-                splay(n);
+                splayOne(n);
                 return n->entry->value;
             }
         }
@@ -740,5 +801,12 @@ int main()
         tree->add(keys[i], keys[i]);
     tree->traverseNLROnSplay(printKey);
     vector<int> x;
-    cout << tree->search(4, x);
+    tree->search(4, x);
+    cout << "!!!!!!!!!" << endl;
+    tree->traverseNLROnSplay(printKey);
+    cout << "!!!!!!!!!!!" << endl;
+    for (auto &it : x)
+    {
+        cout << it << ' ';
+    }
 }
