75fcd48b9445592cd739bf126ec5112608d03bc4
Thanh Vo <thanhvo.dev@gmail.com>
2021-02-25 14:58:19 +0700

    changing var from int to float

--- my-java-game-project/space-invaders/src/NormalEnemy.java
+++ my-java-game-project/space-invaders/src/NormalEnemy.java
@@ -4,24 +4,23 @@ import java.util.*;
 import javax.swing.*; 
 
 public class NormalEnemy extends Rectangle {
-
-	private int health = 1;
-	private int yVelocity;
-	private int xVelocity;
+	
+	private float yVelocity;
+	private float yN;
 	
 	NormalEnemy(int x, int y, int WIDTH, int HEIGHT) {
 		super(x,y,WIDTH,HEIGHT);
-		yVelocity = 3;
+		yVelocity = 0.2f;
+		yN = y;
 	}
 	public void move() {
-		if(GamePanel.timerForEnemy % 20 == 0) {
-			y += yVelocity;
-		}
-		
+			yN += yVelocity;
+			y = (int)yN;
 	}
 	
 	public void draw(Graphics g) {
 		g.setColor(Color.blue);
 		g.fillRect(x, y, width, height);
 	}
+ 
 }
